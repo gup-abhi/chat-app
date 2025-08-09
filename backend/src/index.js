7e6c2315826f6b6aece702c6793d9a381e8cc9ea
Abhishek <agabhishek20121998@gmail.com>
2025-08-08 21:24:07 -0400

    Fixed backend/src/index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,11 +26,11 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
-  // Serve the index.html file for all other routes
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
   });
 }
+
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
