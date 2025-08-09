9d1d53573cc8975278dba6a27e775fdc45ad8fe1
Abhishek <agabhishek20121998@gmail.com>
2025-08-08 21:16:34 -0400

    Fixed vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  
-    NodeGlobalsPolyfillPlugin({
-          buffer: true
-        })
-    ],
+  plugins: [react()]
 })
